c47850151baa68844c7158928d5a0b823aead3bc
jwfing <jwfing@gmail.com>
2024-06-15 15:09:25 +0800

    second version

--- samples/gateway/src/main/java/org/jwfing/samples/gw/Launcher.java
+++ samples/gateway/src/main/java/org/jwfing/samples/gw/Launcher.java
@@ -1,2 +1,42 @@
-package org.jwfing.samples.gw;public class Launcher {
+package org.jwfing.samples.gw;
+
+import io.vertx.core.VertxOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.micrometer.MicrometerMetricsOptions;
+import io.vertx.micrometer.VertxInfluxDbOptions;
+
+public class Launcher extends io.vertx.core.Launcher {
+  private static final Logger logger = LoggerFactory.getLogger(Launcher.class);
+  private static final String INFLUXDB_METRICS_ENABLE = "vertx.metrics.options.influx.enabled";
+  private static final String INFLUXDB_METRICS_URI = "vertx.metrics.options.influx.uri";
+  private static final String INFLUXDB_METRICS_DATABASE = "vertx.metrics.options.influx.db";
+
+  @Override
+  public void beforeStartingVertx(VertxOptions options) {
+    MicrometerMetricsOptions metricsOptions = new MicrometerMetricsOptions().setEnabled(true);
+    String influxMetricsEnable = System.getProperty(INFLUXDB_METRICS_ENABLE);
+    if ("true".equalsIgnoreCase(influxMetricsEnable)) {
+      String influxUri = System.getProperty(INFLUXDB_METRICS_URI, "http://localhost:8086");
+      String influxDb = System.getProperty(INFLUXDB_METRICS_DATABASE, "ayers");
+      VertxInfluxDbOptions influxDbOptions = new VertxInfluxDbOptions().setUri(influxUri)
+              .setDb(influxDb).setEnabled(true);
+      metricsOptions.setInfluxDbOptions(influxDbOptions);
+      logger.info("vertx.metrics.options.influx.options: uri=" + influxUri + ", db=" + influxDb);
+    } else {
+      logger.info("vertx.metrics.options.influx.enabled: false");
+    }
+    options.setMetricsOptions(metricsOptions);
+  }
+
+  public static void main(String[] args) {
+    logger.info("main invoked.");
+    (new Launcher()).dispatch(args);
+  }
+
+  public static void executeCommand(String cmd, String... args) {
+    logger.info("executeCommand invoked.");
+    (new Launcher()).execute(cmd, args);
+  }
 }
--- samples/gateway/src/main/java/org/jwfing/samples/gw/MainVerticle.java
+++ samples/gateway/src/main/java/org/jwfing/samples/gw/MainVerticle.java
@@ -1,2 +1,91 @@
-package org.jwfing.samples.gw;public class MainVerticle {
+package org.jwfing.samples.gw;
+
+import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import org.jwfing.samples.common.HttpVerticle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.vertx.micrometer.MetricsService;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainVerticle extends HttpVerticle {
+  private static final Logger logger = LoggerFactory.getLogger(MainVerticle.class);
+  private static final SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+
+  private HttpServer httpServer = null;
+  private long serverStartTime = 0l;
+  private MetricsService metricsService = null;
+
+  @Override
+  public void start(Promise<Void> startFuture) throws Exception {
+    HttpServerOptions httpServerOptions = new HttpServerOptions()
+            .setTcpFastOpen(true).setTcpCork(true).setTcpQuickAck(true)
+            .addWebSocketSubProtocol("lc.protobuf2.1")
+            .addWebSocketSubProtocol("lc.protobuf2.2")
+            .addWebSocketSubProtocol("lc.protobuf2.3")
+            .setReusePort(true);
+    httpServer = vertx.createHttpServer(httpServerOptions);
+    Router router = Router.router(vertx);
+    router.get("/ping").handler(this::healthcheck);
+    router.get("/metrics").handler(this::outputMetrics);
+
+    int portNum = 8080;
+    httpServer.requestHandler(router).listen(portNum, ar->{
+      if (ar.failed()) {
+        logger.error("can NOT start a http server, cause: ", ar.cause());
+        startFuture.fail(ar.cause());
+      } else {
+        serverStartTime = System.currentTimeMillis();
+        logger.info("http server start at " + getDateString(serverStartTime) + ", listening on port " + portNum);
+        startFuture.complete();
+      }
+    });
+  }
+
+  @Override
+  public void stop(Promise<Void> stopFuture) throws Exception {
+    if (null != httpServer) {
+      httpServer.close(ar -> {
+        logger.info("stop MainVerticle...");
+        stopFuture.complete();
+      });
+    } else {
+      logger.info("stop MainVerticle...");
+      stopFuture.complete();
+    }
+  }
+
+  private String getDateString(long dateTimeStamp) {
+    return dateFormat.format(new Date(dateTimeStamp));
+  }
+
+  private void healthcheck(RoutingContext context) {
+    long currentTs = System.currentTimeMillis();
+    long upTimeInMinute = (currentTs - serverStartTime) / 60000;
+    long upTimeInHour = 0;
+    if (upTimeInMinute > 60) {
+      upTimeInHour = upTimeInMinute / 60l;
+      upTimeInMinute = upTimeInMinute % 60;
+    }
+    JsonObject result = new JsonObject().put("status", "green")
+            .put("availableProcessors", Runtime.getRuntime().availableProcessors())
+            .put("freeMemory", Runtime.getRuntime().freeMemory())
+            .put("totalMemory", Runtime.getRuntime().totalMemory())
+            .put("maxMemory", Runtime.getRuntime().maxMemory())
+            .put("activeThreads", Thread.activeCount())
+            .put("serverStart", getDateString(serverStartTime))
+            .put("upTime", String.format("%d hours %d minutes", upTimeInHour, upTimeInMinute));
+    ok(context, result);
+  }
+
+  private void outputMetrics(RoutingContext context) {
+    JsonObject metrics = metricsService.getMetricsSnapshot();
+    ok(context, metrics);
+  }
 }
